f686cc197e6601bb4794a0d5ac0f8902740662a5
sujanshaikh369 <sush11112011@gmail.com>
2022-01-06 23:25:37 +0600

    Chart and Counter Updated

--- src/components/Shared/Navigation/Navigation.js
+++ src/components/Shared/Navigation/Navigation.js
@@ -25,9 +25,11 @@ const Navigation = () => {
             <Nav.Link as={Link} to={`/explore`}>
               Explore
             </Nav.Link>
-            <Nav.Link as={Link} to={`/dashboard`}>
-              Dashboard
-            </Nav.Link>
+            {user.email && (
+              <Nav.Link as={Link} to={`/dashboard`}>
+                Dashboard
+              </Nav.Link>
+            )}
           </Nav>
           {!user?.email ? (
             <Nav.Link as={Link} to={`/login`}>
